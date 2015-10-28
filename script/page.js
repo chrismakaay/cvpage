0624bf14946db3cb3023cc9e8fdde2252fce6dce
Makkai Krisztián <chrismakaay@gmail.com>
2015-10-28 14:05:53 +0100

    Home section

---
+++ script/page.js
@@ -0,0 +1,21 @@
+$(document).ready(function(ev) {
+    console.log('document ready', Math.floor(Date.now()));
+    
+    setHomeInnerHeight();
+});
+
+$(window).load(function() {
+    console.log('window loaded', Math.floor(Date.now()));
+});
+
+$(window).resize(function() {
+    setHomeInnerHeight();
+});
+
+setHomeInnerHeight = function() {
+    var h = window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
+
+    $('#home').height(h);
+};
